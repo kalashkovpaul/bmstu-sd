289f2e15d2fcd0f1f13fa98f056ce2fc6ef5891b
Pavel Kalashkov <p.kalashkov@vk.team>
2023-06-04 14:18:38 +0300

    DONE

--- src/app/src/logger.ts
+++ src/app/src/logger.ts
@@ -1,6 +1,7 @@
 import { time } from 'console';
 import {createLogger, transports, format } from 'winston';
 import { logConfig } from './configs/log.config';
+import 'winston-daily-rotate-file';
 
 const logger = createLogger({
     level: "info",
@@ -14,6 +15,19 @@ const logger = createLogger({
     transports: [
         new transports.File({filename: logConfig.errorPath, level: "warn"}),
         new transports.File({filename: logConfig.infoPath}),
+        new transports.DailyRotateFile({
+            filename: logConfig.errorPath,
+            level: "warn",
+            datePattern: 'YYYY-MM-DD-HH',
+            zippedArchive: true,
+            maxSize: '20m'
+        }),
+        new transports.DailyRotateFile({
+            filename: logConfig.infoPath,
+            datePattern: 'YYYY-MM-DD-HH',
+            zippedArchive: true,
+            maxSize: '20m'
+        }),
     ],
 });
 
